84ddccc399b55c772565746df9d46fdd63885bb1
Yann Klein <yann.klein@me.com>
2022-05-26 19:12:18 +0900

    initaial commit

---
+++ app.rb
@@ -0,0 +1,10 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "sinatra/activerecord"
+require "pry-byebug"
+require "better_errors"
+require_relative "config/application"
+
+get "/" do
+  "Hello world!"
+end
